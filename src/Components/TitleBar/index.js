7429786db89a32de2268015a44809fee363f967d
Dae Hoon Kim <hunimoda@gmail.com>
2022-01-26 11:26:14 +0900

    Created TitleBar component

---
+++ src/Components/TitleBar/index.js
@@ -0,0 +1,11 @@
+import classes from "./index.module.css";
+
+const TitleBar = () => {
+	return (
+		<div className={classes.titleBar}>
+			<h2 className={classes.title}>받은 편지함</h2>
+		</div>
+	);
+};
+
+export default TitleBar;
--- src/Pages/Inbox/index.js
+++ src/Pages/Inbox/index.js
@@ -1,10 +1,11 @@
+import TitleBar from "../../Components/TitleBar";
 import InboxItem from "../../Components/InboxItem";
 import classes from "./index.module.css";
 
 const Inbox = () => {
 	return (
 		<>
-			<h2>받은 편지함</h2>
+			<TitleBar />
 			<ul className={classes.inboxList}>
 				<InboxItem className={classes.inboxItem} />
 				<InboxItem className={classes.inboxItem} />
