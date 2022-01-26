73fa5a8f14c5edad13533f0ba40f541c9ec92a4a
Dae Hoon Kim <hunimoda@gmail.com>
2022-01-26 17:43:11 +0900

    Copies URL when button clicked, shows check sign if copied

--- src/Components/SentItem/index.js
+++ src/Components/SentItem/index.js
@@ -1,18 +1,42 @@
+import { useState } from "react";
 import LetterItemCard from "../../UI/LetterItemCard";
 import LetterPreview from "../../UI/LetterPreview";
 import classes from "./index.module.css";
 
+const DUMMY_URL = "https://some-application-url/user-id/letter-id";
+
 const SentItem = () => {
+	const [showCopiedOK, setShowCopiedOK] = useState(false);
+
+	const onCopyUrlClick = () => {
+		if (window.isSecureContext) {
+			navigator.clipboard.writeText(DUMMY_URL).then(
+				() => {
+					setShowCopiedOK(true);
+					setTimeout(() => setShowCopiedOK(false), 2000);
+				},
+				() => {
+					alert("Clipboard write failed");
+				}
+			);
+		} else {
+			alert("window.isSecureContext === false");
+		}
+	};
+
+	const copyBtnClassName = `${classes.copyBtn} ${
+		showCopiedOK ? classes["copyBtn--highlight"] : ""
+	}`;
+	const copyIconClassName = showCopiedOK
+		? `fas fa-check ${classes.check}`
+		: "far fa-copy";
+
 	return (
 		<LetterItemCard>
 			<div className={classes.letterURL}>
-				<input
-					type="url"
-					value="https://some-application-url/user-id/letter-id"
-					readOnly
-				/>
-				<button className={classes.copyBtn}>
-					<i className="far fa-copy" />
+				<input type="url" value={DUMMY_URL} readOnly />
+				<button className={copyBtnClassName} onClick={onCopyUrlClick}>
+					<i className={copyIconClassName} />
 				</button>
 			</div>
 			<LetterPreview />
