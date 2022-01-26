11b194074abe6f1bf176d0aa96f6c6aaafe36275
Dae Hoon Kim <hunimoda@gmail.com>
2022-01-26 13:32:13 +0900

    Extracted common features from letter item as LetterPreview UI

--- src/Components/SentItem/index.js
+++ src/Components/SentItem/index.js
@@ -1,7 +1,21 @@
 import LetterItemCard from "../../UI/LetterItemCard";
+import LetterPreview from "../../UI/LetterPreview";
+import classes from "./index.module.css";
 
 const SentItem = () => {
-	return <LetterItemCard>SentItem</LetterItemCard>;
+	return (
+		<LetterItemCard>
+			<div className={classes.row}>
+				<input
+					type="url"
+					value="https://some-application-url/user-id/letter-id"
+					readOnly
+				/>
+				<i className="far fa-copy" />
+			</div>
+			<LetterPreview />
+		</LetterItemCard>
+	);
 };
 
 export default SentItem;
