859822f8d1326abe73e08d9411d7d8c91006703d
Dae Hoon Kim <hunimoda@gmail.com>
2022-01-28 18:08:02 +0900

    Landing page css styling

--- src/Pages/Landing/index.js
+++ src/Pages/Landing/index.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { signInWithGoogle } from "../../Context/auth";
+import classes from "./index.module.css";
 
 const Landing = () => {
 	const dispatch = useDispatch();
@@ -11,14 +12,50 @@ const Landing = () => {
 
 	return (
 		<>
-			<p>ㅇㅇㅇㅇ</p>
-			<p>배경 사진</p>
-			<h1>APP NAME</h1>
-			<p>App description</p>
-			<button>더보기</button>
-			<button onClick={onGoogleLoginClick}>G | 구글 로그인</button>
-			<Link to="/sign-in">이메일로 로그인</Link>
-			<Link to="/sign-up">회원가입</Link>
+			<div className={classes.circleContainer}>
+				<div className={classes.circle} />
+				<div className={classes.circle} />
+				<div className={classes.circle} />
+				<div className={classes.circle} />
+			</div>
+			{/* <div className={classes.bgImage} /> */}
+			<div className={classes.openingText}>
+				<h1 className={classes.appName}>Mailer</h1>
+				<p className={classes.description}>
+					소중한 사람에게 마음을 담아
+					<br />
+					편지를 작성해보세요
+				</p>
+			</div>
+			<div className={classes.bottomContainer}>
+				<button className={classes.openOptionsBtn}>
+					<i className="fas fa-chevron-up" />
+				</button>
+				<div className={classes.loginOptions}>
+					<button onClick={onGoogleLoginClick} className={classes.google}>
+						<span>
+							<i className="fab fa-google" />
+						</span>
+						<span>구글 로그인</span>
+					</button>
+					<button onClick={onGoogleLoginClick} className={classes.kakao}>
+						<span>
+							<i className="fas fa-comment" />
+						</span>
+						<span>카카오 로그인</span>
+					</button>
+					<button onClick={onGoogleLoginClick} className={classes.naver}>
+						<span>
+							<i className="fab fa-neos" />
+						</span>
+						<span>네이버 로그인</span>
+					</button>
+				</div>
+			</div>
+			<div className={classes.links}>
+				<Link to="/sign-in">이메일로 로그인</Link>/
+				<Link to="/sign-up">회원가입</Link>
+			</div>
 		</>
 	);
 };
