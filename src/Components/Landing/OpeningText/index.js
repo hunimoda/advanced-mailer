293cf48a4f364dc42496d3ea0ee2be9adbaa11ea
Dae Hoon Kim <hunimoda@gmail.com>
2022-01-30 17:54:44 +0900

    Modified OpeningText: float effect on text change

--- src/Components/Landing/OpeningText/index.js
+++ src/Components/Landing/OpeningText/index.js
@@ -1,12 +1,20 @@
+import { useEffect, useState } from "react";
 import classes from "./index.module.css";
 
 const OpeningText = ({ className, children }) => {
+	const [isFloating, setIsFloating] = useState(false);
+
+	useEffect(() => {
+		setIsFloating(true);
+		setTimeout(() => setIsFloating(false), 500);
+	}, [children]);
+
 	const openingClassName = `${classes.openingText} ${className}`;
 
 	return (
 		<div className={openingClassName}>
 			<h1>Mailer</h1>
-			<p>{children}</p>
+			<p className={isFloating ? classes.float : null}>{children}</p>
 		</div>
 	);
 };
