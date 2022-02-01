f28bf783854b38465c896e83e4ed37ddd98bd2d9
Dae Hoon Kim <hunimoda@gmail.com>
2022-02-01 14:07:32 +0900

    Adjust sheet size according to screen orientation

--- src/Pages/Letter/index.js
+++ src/Pages/Letter/index.js
@@ -1,19 +1,69 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getLetter } from "../../Firebase/db";
 
 const Letter = () => {
-	const { sender, letter } = useParams();
+	const { sender, letter: letterId } = useParams();
+	const [sheetSize, setSheetSize] = useState(null);
+
+	console.log(sheetSize);
 
 	useEffect(() => {
-		getLetter(sender, letter).then((data) => {
-			if (data) {
-				console.log(data);
+		getLetter(sender, letterId).then((letter) => {
+			if (letter) {
+				const { aspectRatio: sheetAspectRatio, objects } = letter;
+				const { screen } = window;
+
+				const updateSheetSize = () => {
+					const { width: screenWidth, height: screenHeight } = screen;
+					const screenAspectRatio = screenWidth / screenHeight;
+
+					let sheetWidth;
+					let sheetHeight;
+
+					if (sheetAspectRatio > screenAspectRatio) {
+						sheetWidth = screenWidth;
+						sheetHeight = sheetWidth / sheetAspectRatio;
+					} else {
+						sheetHeight = screenHeight;
+						sheetWidth = sheetHeight * sheetAspectRatio;
+					}
+
+					setSheetSize({ width: sheetWidth, height: sheetHeight });
+				};
+
+				updateSheetSize();
+
+				screen.orientation.addEventListener("change", () => {
+					updateSheetSize();
+				});
+			} else {
+				// Show error message: invalid URL
+				console.log("No data!");
 			}
 		});
-	}, [sender, letter]);
+	}, [sender, letterId]);
 
-	return <p>{`sender: ${sender}, letter: ${letter}`}</p>;
+	if (sheetSize) {
+		return (
+			<>
+				<div style={{ background: "black", width: "100%", height: "100vh" }} />
+				<div
+					style={{
+						position: "fixed",
+						top: "50%",
+						left: "50%",
+						transform: "translate(-50%, -50%)",
+						width: `${sheetSize.width}px`,
+						height: `${sheetSize.height}px`,
+						background: "white",
+					}}
+				/>
+			</>
+		);
+	} else {
+		return null;
+	}
 };
 
 export default Letter;
