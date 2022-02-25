fe90b44297ae416c2bd1be57d5ff0ba5102ecc2f
Dae Hoon Kim <hunimoda@gmail.com>
2022-02-26 00:21:10 +0900

    Modified TitleBar component: spin refresh button on pending

--- src/Components/TitleBar/index.js
+++ src/Components/TitleBar/index.js
@@ -1,16 +1,22 @@
-import MoreLetters from "../MoreLetters";
+import { useEffect, useState } from "react";
 import classes from "./index.module.css";
 
 const TitleBar = ({ children, onRefresh, status }) => {
+	const [titleBarClass, setTitleBarClass] = useState(classes.titleBar);
+
+	useEffect(() => {
+		if (status === "pending" && titleBarClass === classes.titleBar) {
+			setTitleBarClass(classes.titleBar + " " + classes["titleBar--pending"]);
+		} else if (status !== "pending" && titleBarClass !== classes.titleBar) {
+			setTimeout(() => setTitleBarClass(classes.titleBar), 1000);
+		}
+	}, [status, titleBarClass]);
+
 	return (
-		<div className={classes.titleBar}>
+		<div className={titleBarClass}>
 			<h2 className={classes.title}>{children}</h2>
 			<button onClick={onRefresh} className={classes.refreshButton}>
-				{status === "pending" ? (
-					<MoreLetters status={status} />
-				) : (
-					<i className="fas fa-sync-alt" />
-				)}
+				<i className="fas fa-sync-alt" />
 			</button>
 		</div>
 	);
