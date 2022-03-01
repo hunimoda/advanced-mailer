d90c517382656dc7c77245b16769c9d6c5b3a73c
Dae Hoon Kim <hunimoda@gmail.com>
2022-03-01 14:15:34 +0900

    Modified Canvas: changed draw method from lineTo to quadraticCurveTo

--- src/Components/New/ToolBox/Canvas/index.js
+++ src/Components/New/ToolBox/Canvas/index.js
@@ -3,8 +3,9 @@ import { createPortal } from "react-dom";
 import { useSelector } from "react-redux";
 import classes from "./index.module.css";
 
+const SCALE_FACTOR = 1;
 let context = null;
-let prevCoord = null;
+let coords = [];
 
 const Canvas = () => {
 	const canvasRef = useRef();
@@ -16,12 +17,13 @@ const Canvas = () => {
 	useEffect(() => {
 		const canvas = canvasRef.current;
 
-		canvas.width = width;
-		canvas.height = height;
+		canvas.width = width * SCALE_FACTOR;
+		canvas.height = height * SCALE_FACTOR;
 
 		context = canvas.getContext("2d");
 		context.strokeStyle = "red";
 		context.lineCap = "round";
+		context.lineJoin = "round";
 		// context.lineWidth = 2.5;
 	}, [width, height]);
 
@@ -31,6 +33,13 @@ const Canvas = () => {
 		return {
 			x: event.touches[0].clientX - canvas.x,
 			y: event.touches[0].clientY - canvas.y,
+		};
+	};
+
+	const getMiddlePosition = (pos1, pos2) => {
+		return {
+			x: (pos1.x + pos2.x) / 2,
+			y: (pos1.y + pos2.y) / 2,
 		};
 	};
 
@@ -45,8 +54,12 @@ const Canvas = () => {
 
 	const onTouchStart = (event) => {
 		event.preventDefault();
+
+		if (pointerType !== "pen") {
+			return;
+		}
 		// context.beginPath();
-		prevCoord = getTouchPositionFromEvent(event);
+		coords.push(getTouchPositionFromEvent(event));
 	};
 
 	const onTouchMove = (event) => {
@@ -56,19 +69,46 @@ const Canvas = () => {
 			return;
 		}
 
-		const currCoord = getTouchPositionFromEvent(event);
+		const newCoord = getTouchPositionFromEvent(event);
+		const midCoord = getMiddlePosition(coords[coords.length - 1], newCoord);
+
+		coords.push(midCoord, newCoord);
+		coords = coords.slice(-4);
 
 		context.beginPath();
-		context.moveTo(prevCoord.x, prevCoord.y);
-		context.lineTo(currCoord.x, currCoord.y);
-		context.lineWidth = Math.min(12 * pressure, 3);
-		context.stroke();
+		context.moveTo(SCALE_FACTOR * coords[0].x, SCALE_FACTOR * coords[0].y);
 
-		prevCoord = currCoord;
+		if (coords.length < 4) {
+			context.lineTo(SCALE_FACTOR * coords[1].x, SCALE_FACTOR * coords[1].y);
+		} else {
+			context.quadraticCurveTo(
+				SCALE_FACTOR * coords[1].x,
+				SCALE_FACTOR * coords[1].y,
+				SCALE_FACTOR * coords[2].x,
+				SCALE_FACTOR * coords[2].y
+			);
+		}
+		context.lineWidth = SCALE_FACTOR * Math.max(1, Math.min(12 * pressure, 3));
+		context.stroke();
 	};
 
 	const onTouchEnd = (event) => {
 		event.preventDefault();
+
+		if (pointerType !== "pen") {
+			return;
+		}
+
+		coords = coords.slice(-2);
+		if (coords.length === 2) {
+			context.beginPath();
+			context.moveTo(SCALE_FACTOR * coords[0].x, SCALE_FACTOR * coords[0].y);
+			context.lineTo(SCALE_FACTOR * coords[1].x, SCALE_FACTOR * coords[1].y);
+			context.lineWidth =
+				SCALE_FACTOR * Math.max(1, Math.min(12 * pressure, 3));
+			context.stroke();
+		}
+		coords = [];
 	};
 
 	return createPortal(
