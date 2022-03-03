732f7f95b870c5782218603337c834d54d56d999
Dae Hoon Kim <hunimoda@gmail.com>
2022-03-03 12:37:18 +0900

    Completed StyleToolbox: select one of the two styles (constant, varying)

--- src/Components/New/TopHeader/StyleToolbox/index.js
+++ src/Components/New/TopHeader/StyleToolbox/index.js
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import Container from "../Container";
 import Row from "../Row";
+import classes from "./index.module.css";
+
+const BRUSH_STYLES = ["constant", "varying"];
 
 const StyleToolbox = () => {
+	const [brushStyle, setBrushStyle] = useState(BRUSH_STYLES[0]);
+
+	const onSelectStyle = (event) =>
+		setBrushStyle(event.currentTarget.dataset.label);
+
 	return (
 		<div>
 			<Row>
-				<Container type="long" />
-				<Container type="long" />
+				{BRUSH_STYLES.map((style) => (
+					<Container
+						key={style}
+						type="long"
+						label={style}
+						onClick={onSelectStyle}
+						selected={style === brushStyle}
+					>
+						<img
+							src={`/${style}-width.png`}
+							alt=""
+							className={classes.brushStyleImage}
+						/>
+					</Container>
+				))}
 			</Row>
 		</div>
 	);
